b0dd437af507595d82e09b86ef79f884e86277da
IgorTargino <igortargino01@gmail.com>
2021-06-22 20:20:39 -0300

    feat: hook useAuth

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
 
 import { BrowserRouter, Route } from 'react-router-dom';
-import { AuthContextProvider } from './context/authContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 import './styles/global.scss';
 
