2dee2d6a956c5e0de1ec5f3520d1376c96155e22
IgorTargino <igortargino01@gmail.com>
2021-06-24 22:38:35 -0300

    feat: style of buttons

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import cx from "classnames";
 
-import './styles.scss';
+import "./styles.scss";
 
 type QuestionProps = {
   content: string;
@@ -9,21 +10,35 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
-}
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+};
 
-const Question = ({ author, content, children }: QuestionProps) => {
+const Question = ({
+  author,
+  content,
+  children,
+  isAnswered = false,
+  isHighlighted = false,
+}: QuestionProps) => {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered}
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div>{ children }</div>
+        <div>{children}</div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -5,7 +5,7 @@ import { database } from "../../services/firebase";
 import { RoomCode, Question, Button } from "../../components";
 
 import logoImg from "../../assets/images/logo.svg";
-import deleteImg from '../../assets/images/delete.svg';
+import deleteImg from "../../assets/images/delete.svg";
 
 import "./styles.scss";
 
@@ -17,33 +17,63 @@ const AdminRoom = () => {
   const params = useParams<RoomParams>();
   const history = useHistory();
   const roomId = params.id;
-  
+
   const { questions, title } = useRoom(roomId);
 
   const handleEndRoom = async () => {
     await database.ref(`/rooms/${roomId}`).update({
       endedAt: new Date(),
-    })
+    });
 
-    history.push('/');
-  }
-
-  const handleDeleteQuestion = async (questionId: string) => {
-    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
-      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
-    }
-  }
+    history.push("/");
+  };
 
   const moveToHome = () => {
-    history.push('/');
-  }
+    history.push("/");
+  };
 
+  const handleCheckQuestionAsAnswered = async (
+    questionId: string,
+    isAnswered: boolean
+  ) => {
+    if (isAnswered) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isAnswered: false,
+      });
+    } else {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isAnswered: true,
+        isHighlighted: false,
+      });
+    }
+  };
+
+  const handleHighlightQuestion = async (
+    questionId: string,
+    isHighlighted: boolean
+  ) => {
+    if (isHighlighted) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isHighlighted: false,
+      });
+    } else {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isHighlighted: true,
+      });
+    }
+  };
+
+  const handleDeleteQuestion = async (questionId: string) => {
+    if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  };
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
-          <img src={logoImg} alt="Letmeask" onClick={moveToHome}/>
+          <img src={logoImg} alt="Letmeask" onClick={moveToHome} />
           <div>
             <RoomCode code={roomId} />
             <Button onClick={handleEndRoom}>Encerrar sala</Button>
@@ -54,16 +84,84 @@ const AdminRoom = () => {
         <div className="room-title">
           <h1>Sala {title}</h1>
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
-         
         </div>
         <div className="question-list">
-          {questions.map(question => {
+          {questions.map((question) => {
             return (
-              <Question 
+              <Question
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                <button
+                  type="button"
+                  className={`${question.isAnswered ? "active" : ""}`}
+                  onClick={() =>
+                    handleCheckQuestionAsAnswered(
+                      question.id,
+                      question.isAnswered
+                    )
+                  }
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="12.0003"
+                      cy="11.9998"
+                      r="9.00375"
+                      stroke="#737380"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193"
+                      stroke="#737380"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+
+                {!question.isAnswered && (
+                  <button
+                    type="button"
+                    className={`${question.isHighlighted ? "active" : ""}`}
+                    onClick={() =>
+                      handleHighlightQuestion(
+                        question.id,
+                        question.isHighlighted
+                      )
+                    }
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                )}
+
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
