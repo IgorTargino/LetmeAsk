8688f59cc3954fe6ebfb7c4c4af0e65d62f7e6b5
igortargino <igortargino01@gmail.com>
2021-06-27 01:18:15 -0300

    fix: recolor purple dark mode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -60,8 +60,8 @@ const Home = () => {
           src={illustrationImg}
           alt="Ilustração simbolizando perguntas e respostas"
         />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo real</p>
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
       <main>
         <div className={styles.mainContent}>
