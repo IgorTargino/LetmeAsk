2a4faf1105cec770853fa70bccfdf3a2a7f17de5
igortargino <igortargino01@gmail.com>
2021-06-29 23:09:32 -0300

    feat: userProfile  component

---
+++ src/components/UserProfile/index.tsx
@@ -0,0 +1,27 @@
+import { useHistory } from "react-router";
+
+import useAuth from "../../hooks/useAuth";
+
+import styles from './styles.module.scss';
+
+const UserProfile = () => {
+  const {user, signOut} = useAuth();
+  const history = useHistory();
+
+  const singOutAccount = async () => {
+    if (user) {
+      await signOut();
+      history.push('/');
+    }
+  }
+
+  return (
+    <div className={styles.userInfo}>
+      <img src={user?.avatar} alt={user?.name} />
+      <span>{user?.name}</span>
+      <button onClick={singOutAccount}>(sair)</button>
+    </div>
+  );
+};
+
+export default UserProfile;
